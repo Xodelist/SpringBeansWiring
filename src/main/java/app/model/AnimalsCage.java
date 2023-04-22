339de5d28e094b19c720d4f4acf78b88a6d79814
Xodelist <122819269+Xodelist@users.noreply.github.com>
2023-04-22 12:07:53 +0300

    Add files via upload

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,36 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+
+    private Animal animal;
+
+    private Timer timer;
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    public AnimalsCage(@Qualifier(value = "dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
